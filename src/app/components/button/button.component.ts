10898d02e85a3c8b45642c4b8ec55b597cadd36f
Mateus de Moura Pragana <mateuspragana@gmail.com>
2025-01-25 16:37:32 -0300

    feat: creating button component

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Input() text: string = ""
+  @Input() type: string = ""
+}
