841400e43a96d2a1e337401f283556fa8bfc560a
Mateus de Moura Pragana <mateuspragana@gmail.com>
2025-01-25 16:37:13 -0300

    feat: setting up icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { provideIcons } from '@ng-icons/core';
+import { ionLockClosed, ionMail } from "@ng-icons/ionicons";
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  viewProviders: [provideIcons({ ionLockClosed, ionMail })],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
