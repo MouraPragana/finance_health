8d2d5b48940d0fdc6fb3043e7f17bf097c000493
Mateus de Moura Pragana <mateuspragana@gmail.com>
2025-01-26 06:42:54 -0300

    feat: refactoring input component and coding fowardref input form

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,17 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-input',
-  imports: [CommonModule, NgIcon],
+  imports: [CommonModule, NgIcon, ReactiveFormsModule],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  styleUrl: './input.component.css',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => InputComponent),
+    multi: true
+  }]
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor  {
   @Input() title: string = ""
   @Input() name: string = ""
   @Input() type: string = ""
   @Input() placeholder: string = ""
   @Input() icon: string = ""
+
+  value: string = ""
+  onChange: any = () => {}
+  onTouched: any = () => {}
+
+  onInput(event: Event){
+    const value = (event.target as HTMLInputElement).value
+    this.onChange(value)
+  }
+
+  writeValue(value: any): void {
+    this.value = value
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn
+  }
 }
