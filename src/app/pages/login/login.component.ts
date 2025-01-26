00ba8e51076be58cea4e32b1c62c3c6b403c0c14
Mateus de Moura Pragana <mateuspragana@gmail.com>
2025-01-26 06:43:25 -0300

    refactor: refactoring login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
-import { InputComponent } from "../../components/input/input.component";
-import { ButtonComponent } from "../../components/button/button.component";
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DefaultLayoutHomeComponent } from "../../components/default-layout-home/default-layout-home.component";
+import { InputComponent } from '../../components/input/input.component';
+import { CommonModule } from '@angular/common';
+interface LoginForm{
+  login: FormControl,
+  password: FormControl
+}
 @Component({
   selector: 'app-login',
-  imports: [InputComponent, ButtonComponent],
+  imports: [ReactiveFormsModule, DefaultLayoutHomeComponent, InputComponent, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup<LoginForm>;
 
+  constructor(private router: Router){
+    this.loginForm = new FormGroup({
+      login: new FormControl(""),
+      password: new FormControl("")
+    })
+  }
+
+  submit(){
+    console.log(this.loginForm.value.login, this.loginForm.value.password)
+  }
+
+  redirect(path: string){
+    this.router.navigate([path]);
+  }
 }
