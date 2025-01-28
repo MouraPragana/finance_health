59c73fba7859fae1c12263ac828556856f04cf32
Mateus de Moura Pragana <mateuspragana@gmail.com>
2025-01-28 15:27:38 -0300

    feat: adding interceptors, animations and toastr to appconfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
+import { addTokenRequest } from './interceptors/interceptors.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withFetch())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+     provideRouter(routes),
+      provideHttpClient(withFetch(),
+      withInterceptors([addTokenRequest])),
+      provideAnimations(),
+      provideToastr()]
 };
+
