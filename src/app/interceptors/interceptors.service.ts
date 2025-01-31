8530ae9c26dd6f8cc2daaf09e73da3387875f01d
Mateus de Moura Pragana <mateuspragana@gmail.com>
2025-01-31 15:08:31 -0300

    feat: setting up global interceptor

--- src/app/interceptors/interceptors.service.ts
+++ src/app/interceptors/interceptors.service.ts
@@ -3,12 +3,17 @@ import { inject } from "@angular/core";
 import { Observable } from "rxjs";
 import { TokenService } from "../services/token.service";
 
-export function addTokenRequest(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
-  const authToken = inject(TokenService).getAuthToken();
+export function execGlobalInteceptor(req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+  const token = inject(TokenService);
+  const authToken = token.getAuthToken();
+  const refresh_token = token.getRefreshToken();
 
   if (authToken){
+    const modifiedBody = {...req.body, refresh_token}
+
     const newReq = req.clone({
-      headers: req.headers.append("authorization", `Bearer ${authToken}`)
+      headers: req.headers.append("authorization", `Bearer ${authToken}`),
+      body: modifiedBody
     })
 
     return next(newReq)
